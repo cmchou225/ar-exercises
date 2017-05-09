3f0edc1eb2fcd1b76e61f4c976d67369b5b4d3fd
Charles <cming.chou@gmail.com>
2017-05-09 20:35:25 +0000

    ex 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+p Store.sum(:annual_revenue)
+p Store.sum(:annual_revenue) / Store.count
+p Store.where('annual_revenue >= ?', 1000000 ).count
