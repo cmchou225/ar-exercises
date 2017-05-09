edf356b24f5618d7104d0183a16b140a5ed7f02a
Charles <cming.chou@gmail.com>
2017-05-09 21:42:16 +0000

    ex 7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,6 +10,7 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name:"Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "John", last_name:"Smith", hourly_rate: 20)
-@store2.employees.create(first_name: "Steve", last_name:"Veck", hourly_rate: 30)
-@store2.employees.create(first_name: "Jack", last_name:"Spacey", hourly_rate: 50)
+@store1.employees.create(first_name: "John", last_name:"Smith", hourly_rate: 40)
+@store2.employees.create(first_name: "Steve", last_name:"Veck", hourly_rate: 40)
+@store2.employees.create(first_name: "Jack", last_name:"Beck", hourly_rate: 50)
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, :greater_than_or_equal_to => 0}
+
+  validate :must_sell_men_or_women_clothing, on: :create
+
+  def must_sell_men_or_women_clothing
+     if !mens_apparel && !womens_apparel
+       errors.add(:base, "must sell either mens or womens apparel")
+      end
+  end
+
 end
