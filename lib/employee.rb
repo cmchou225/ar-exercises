925c1a74b59d78609227c12d0790be39dae5c34d
Charles <cming.chou@gmail.com>
2017-05-10 01:12:18 +0000

    stretch completed

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,6 +3,13 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store, presence: true
   validates_inclusion_of :hourly_rate, :in => 40..200
 
-  
+  before_create do
+    self.password = random_string
+  end
 
+  private
+  
+  def random_string
+    'password'
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -5,10 +5,20 @@ class Store < ActiveRecord::Base
 
   validate :must_sell_men_or_women_clothing, on: :create
 
+  before_destroy :employee_check, prepend: true
+
+  private
+
   def must_sell_men_or_women_clothing
      if !mens_apparel && !womens_apparel
        errors.add(:base, "must sell either mens or womens apparel")
       end
+  end
+
+  def employee_check
+    if self.employees.count > 0
+      throw :abort
+    end
   end
 
 end
